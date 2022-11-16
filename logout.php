3d05caca90273dceeae02a94c9b7cc393465f689
Med_ZAROUAL <55965140+Med-Zaroual@users.noreply.github.com>
2022-11-16 12:29:42 +0100

    nothing changed

--- logout.php
+++ logout.php
@@ -2,7 +2,11 @@
 include ('dbcon.php');
 session_start();
 session_destroy();
+
 $session_id=$_SESSION['user_id'];
+
 $request=mysqli_query($con,"update Users set status='0' where user_id='$session_id'");
 header('location:index.php');
+
+
 ?>
