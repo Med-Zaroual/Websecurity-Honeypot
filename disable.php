3701b7fe6b55f254c70716bcc3ebb5509f861933
Med_ZAROUAL <55965140+Med-Zaroual@users.noreply.github.com>
2022-11-23 21:46:13 +0100

    disable.php secured

--- disable.php
+++ disable.php
@@ -4,11 +4,14 @@ include("dbcon.php");
 
 if(isset($_GET['user'])){
 	$id=$_GET['user'];
-	$user=getUserById($id)
-	if($user['user_type']!=="admin"){
+	$user=getUserById($id);
+	if($user['user_type'] !== "admin"){
 		disable_user($id);
 		header('location:admin.php');
-	}	
+	}
+	else{
+		header('location:error_403.php');
+	}		
 }
 
 ?>
